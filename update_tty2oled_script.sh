e43fa0571cd9490f9d53847c01785c452f0399f4
Oliver Jaksch <ojaksch@com-in.de>
2021-04-08 12:51:04 +0200

    Update to the update scripts

--- update_tty2oled_script.sh
+++ update_tty2oled_script.sh
@@ -28,8 +28,10 @@
 #REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/main"
 REPOSITORY_URL="https://github.com/venice1200/MiSTer_tty2oled/raw/main"
 TTY2OLED_PATH="/media/fat/tty2oledpics"
-#NODEBUG="-q"
-NODEBUG="-o /dev/null"
+NODEBUG="-q -o /dev/null"
+
+# Check and remount root writable if neccessary
+[ $(/bin/mount | head -n1 | grep -c "(ro,") = 1 ] && /bin/mount -o remount,rw /
 
 echo -e "\n\e[1;32mtty2oled update script"
 echo -e "----------------------\e[0m"

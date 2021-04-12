3c0fd73513c35ba79b6052bee2f28071e063cf87
Oliver Jaksch <ojaksch@com-in.de>
2021-04-12 15:39:19 +0200

    Update to the update scripts

--- update_tty2oled.sh
+++ update_tty2oled.sh
@@ -26,8 +26,7 @@
 
 
 #REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/main"
-#REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/testing"		# Testing branch
-REPOSITORY_URL="https://raw.githubusercontent.com/ojaksch/MiSTer_tty2oled/master"
+REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/testing"		# Testing branch
 SCRIPTNAME="/tmp/update_tty2oled_script.sh"
 NODEBUG="-q -o /dev/null"
 

c860db16434e301f42497bc9af98de9c0ffe5284
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-04-13 07:49:17 +0200

    Update update_tty2oled.sh

--- update_tty2oled.sh
+++ update_tty2oled.sh
@@ -31,7 +31,7 @@ SCRIPTNAME="/tmp/update_tty2oled_script.sh"
 NODEBUG="-q -o /dev/null"
 
 # ! [[ -f /media/fat/Scripts/tty2oled.ini ]] && wget ${NODEBUG} --no-cache "${REPOSITORY_URL}/tty2oled.ini" -O /media/fat/Scripts/tty2oled.ini
-if ! [ -f "${picturefolder}/${1}.xbm" ]; then
+if ! [ -f /media/fat/Scripts/tty2oled.ini ]; then
   echo -e "\e[1;33mDownloading INI File \e[1;35m${PICNAME}\e[0m"
   wget ${NODEBUG} --no-cache "${REPOSITORY_URL}/tty2oled.ini" -O /media/fat/Scripts/tty2oled.ini
 fi

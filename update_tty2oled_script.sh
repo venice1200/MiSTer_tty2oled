b602e64a263aa2ca0ce0461c65d2bfc5d4ef087b
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-06-22 16:52:46 +0200

    Fixes Typo

--- update_tty2oled_script.sh
+++ update_tty2oled_script.sh
@@ -109,7 +109,7 @@ if [ "${USBMODE}" = "yes" ]; then
     done
   else
     echo -e "\e[1;33mSkipping Text-Based Picture download because of USE_US_PICTURES INI-Option \e[1;35m${PICNAME}\e[0m"
-  1fi
+  fi
 else
   echo -e "\e[1;33mSkipping Picture Download because of USBMODE INI-Option \e[1;35m${PICNAME}\e[0m"
 fi

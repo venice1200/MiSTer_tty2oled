f15f3d8bbb4332f7f1f95591b1e3db9562cda244
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-06-22 16:57:41 +0200

    Update update_tty2oled_script.sh

--- update_tty2oled_script.sh
+++ update_tty2oled_script.sh
@@ -78,7 +78,7 @@ if [ "${USBMODE}" = "yes" ]; then
       fi
     done
   else
-    echo -e "\e[1;33mSkipping Text-Based Picture download because of USE_TEXT_PICTURES INI-Option \e[1;35m${PICNAME}\e[0m"
+    echo -e "\e[1;33mSkipping Text-Based Picture download because of USE_TEXT_PICTURE INI-Option \e[1;35m${PICNAME}\e[0m"
   fi
   
   # Graphic-Based Pictures (as Second = Higher Priority)
@@ -108,7 +108,7 @@ if [ "${USBMODE}" = "yes" ]; then
       fi
     done
   else
-    echo -e "\e[1;33mSkipping Text-Based Picture download because of USE_US_PICTURES INI-Option \e[1;35m${PICNAME}\e[0m"
+    echo -e "\e[1;33mSkipping Text-Based Picture download because of USE_US_PICTURE INI-Option \e[1;35m${PICNAME}\e[0m"
   fi
 else
   echo -e "\e[1;33mSkipping Picture Download because of USBMODE INI-Option \e[1;35m${PICNAME}\e[0m"

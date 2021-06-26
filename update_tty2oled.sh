4a1d35dc7d6f22367f968a3e83390b2929516297
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-06-26 20:15:31 +0200

    Update update_tty2oled.sh

--- update_tty2oled.sh
+++ update_tty2oled.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# v1.2 - Copyright (c) 2021 RealLarry, venice
+# v1.2 - Copyright (c) 2021 ojaksch, venice
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

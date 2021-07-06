6292e22dd7e99ae537572a3adce5215b8a7e7b6a
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-07-06 12:19:31 +0200

    Update update_tty2oled_script.sh

--- update_tty2oled_script.sh
+++ update_tty2oled_script.sh
@@ -59,7 +59,7 @@ elif ! cmp -s /tmp/S60tty2oled ${INITSCRIPT}; then
     mv -f /tmp/S60tty2oled ${INITSCRIPT}
     chmod +x ${INITSCRIPT}
   else
-    echo -e "\e[5;31mSkipping \e[25;1;33mavailable init script update because of \e[1;94m$SCRIPT_UPDATE\e[1;33m INI-Option\e[0m"
+    echo -e "\e[5;31mSkipping\e[25;1;33m available init script update because of \e[1;94m$SCRIPT_UPDATE\e[1;33m INI-Option\e[0m"
   fi
 fi
 [[ -f /tmp/S60tty2oled ]] && rm /tmp/S60tty2oled
@@ -77,7 +77,7 @@ elif ! cmp -s /tmp/tty2oled ${DAEMONSCRIPT}; then
     mv -f /tmp/tty2oled ${DAEMONSCRIPT}
     chmod +x ${DAEMONSCRIPT}
   else
-    echo -e "\e[5;31mSkipping \e[25;1;33mavailable daemon script update because of \e[1;94m$SCRIPT_UPDATE\e[1;33m INI-Option\e[0m"
+    echo -e "\e[5;31mSkipping\e[25;1;33m available daemon script update because of \e[1;94m$SCRIPT_UPDATE\e[1;33m INI-Option\e[0m"
   fi
 fi
 [[ -f /tmp/tty2oled ]] && rm /tmp/tty2oled
@@ -133,7 +133,7 @@ if [ "${USBMODE}" = "yes" ]; then
     echo -e "\e[1;33mSkipping US-Version Picture download because of \e[1;94m$USE_US_PICTURE\e[1;33m INI-Option\e[0m"
   fi
 else
-  echo -e "\e[5;31mSkipping \e[25;1;33mPicture Download because of \e[1;94m$USBMODE\e[1;33m INI-Option\e[0m"
+  echo -e "\e[5;31mSkipping\e[25;1;33m Picture Download because of \e[1;94m$USBMODE\e[1;33m INI-Option\e[0m"
 fi
 
 sync

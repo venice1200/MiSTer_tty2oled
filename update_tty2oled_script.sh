cf7a50b162ec25eafc3e7964d9da9bbf69d58036
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-07-06 09:03:47 +0200

    Update update_tty2oled_script.sh

--- update_tty2oled_script.sh
+++ update_tty2oled_script.sh
@@ -52,7 +52,8 @@ echo -e "\e[1;32mChecking for available updates...\e[0m"
 
 # init script
 wget ${NODEBUG} "${REPOSITORY_URL}/S60tty2oled" -O /tmp/S60tty2oled
-if ! cmp -s /tmp/S60tty2oled ${INITSCRIPT}; then
+#if ! cmp -s /tmp/S60tty2oled ${INITSCRIPT}; then
+if  ! [ -f ${INITSCRIPT} ] || ( ! cmp -s /tmp/S60tty2oled ${INITSCRIPT} &&  [ "${SCRIPT_UPDATE}" = "yes" ] ); then bzw.
   echo -e "\e[1;33mUpdating init script \e[1;35mS60tty2oled\e[0m"
   mv -f /tmp/S60tty2oled ${INITSCRIPT}
   chmod +x ${INITSCRIPT}
@@ -62,8 +63,9 @@ fi
 
 # daemon
 wget ${NODEBUG} "${REPOSITORY_URL}/tty2oled" -O /tmp/tty2oled
-if ! cmp -s /tmp/tty2oled ${DAEMONSCRIPT}; then
-  echo -e "\e[1;33mUpdating daemon \e[1;35mtty2oled\e[0m"
+#if ! cmp -s /tmp/tty2oled ${DAEMONSCRIPT}; then
+if  ! [ -f ${DAEMONSCRIPT} ] || ( ! cmp -s /tmp/tty2oled ${DAEMONSCRIPT} &&  [ "${SCRIPT_UPDATE}" = "yes" ] ); then
+echo -e "\e[1;33mUpdating daemon \e[1;35mtty2oled\e[0m"
   mv -f /tmp/tty2oled ${DAEMONSCRIPT}
   chmod +x ${DAEMONSCRIPT}
 else

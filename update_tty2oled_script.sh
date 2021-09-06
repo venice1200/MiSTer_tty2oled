eeb98583d45ec84753e9f99599b739736106b808
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-09-06 11:19:59 +0200

    Update update_tty2oled_script.sh

--- update_tty2oled_script.sh
+++ update_tty2oled_script.sh
@@ -76,7 +76,7 @@ if [ -d /media/fat/tty2oledpics ]; then
   rm -rf /media/fat/tty2oledpics/
 fi
 
-echo -e "\e[1;34mtty2oled\e[1;32m update script"
+echo -e "\e[1;32mtty2oled update script"
 echo -e "----------------------\e[0m"
 
 echo -e "\e[1;32mChecking for available tty2oled updates...\e[0m"

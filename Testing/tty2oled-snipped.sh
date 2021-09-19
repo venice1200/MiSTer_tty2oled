adb2ba218c564538c22d455fd0ea3310423ca74b
Oliver Jaksch <ojaksch@com-in.de>
2021-09-19 05:40:21 +0200

    Cleanup of tty2oled-snipped.sh

--- Testing/tty2oled-snipped.sh
+++ Testing/tty2oled-snipped.sh
@@ -1,27 +1,24 @@
 # USB Send-Picture-Data function
 senddata() {
   if [ "${USBMODE}" = "yes" ]; then                                             # Check the tty2xxx mode
-    if [ -f "${picturefolder_pri}/${1}.xbm" ]; then                             # Lookup for an existing XBM in PRI and proceed
+    unset PICFNAM
+    if [ -f "${picturefolder_pri}/${1}.gsc" ]; then                             # Lookup for an existing GSC in PRI and proceed
+      PICFNAM="${picturefolder_pri}/${1}.gsc"
+    elif [ -f "${picturefolder_pri}/${1}.xbm" ]; then                           # Lookup for an existing XBM in PRI and proceed
       PICFNAM="${picturefolder_pri}/${1}.xbm"
-      SENDPIC="yes"
-    elif [ -f "${picturefolder_16gray}/${1}.xbm" ]; then                        # Lookup for an existing XBM and proceed
-      PICFNAM="${picturefolder_16gray}/${1}.xbm"
-      SENDPIC="yes"
+    elif [ -f "${picturefolder_gsc}/${1}.gsc" ]; then                           # Lookup for an existing GSC and proceed
+      PICFNAM="${picturefolder_gsc}/${1}.gsc"
     elif [ -f "${picturefolder}/${1}.xbm" ]; then                               # Lookup for an existing XBM and proceed
       PICFNAM="${picturefolder}/${1}.xbm"
-      SENDPIC="yes"
     fi                                                                          # End if Picture check
-    if [ "${SENDPIC}" = "yes" ]; then
+    if [[ -n ${PICFNAM} ]]; then
       dbug "Sending: CMDCOR,${1}"
-      tail -n +4 "${PICFNAM}" | xxd -r -p > /dev/shm/corepic
-      PICSIZE=$(stat -c "%s" /dev/shm/corname)
+      # PICSIZE=$(stat -c "%s" "${PICFNAM}")
       echo "CMDCOR,${1}" > ${TTYDEV}                                            # Send CORECHANGE" Command and Corename
       sleep ${WAITSECS}                                                         # sleep needed here ?!
-      echo ${PICSIZE} > ${TTYDEV}                                               # Send CORECHANGE" Command and Corename
-      sleep ${WAITSECS}                                                         # sleep needed here ?!
-      cat /dev/shm/corepic > ${TTYDEV}                                          # The Magic, send the Picture-Data up from Line 4 and proces$
-      rm /dev/shm/corepic
-      unset SENDPIC
+      # echo ${PICSIZE} > ${TTYDEV}                                               # Send CORECHANGE" Command and Corename
+      # sleep ${WAITSECS}                                                         # sleep needed here ?!
+      tail -n +4 "${PICFNAM}" | xxd -r -p > ${TTYDEV}                           # /dev/shm
     else                                                                        # No Picture available!
       echo "${1}" > ${TTYDEV}                                                   # Send just the CORENAME
     fi

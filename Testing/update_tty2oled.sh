02e84c96a8ddfde451acabd8ad8db69e698f8376
Oliver Jaksch <ojaksch@com-in.de>
2021-09-23 09:40:20 +0200

    Testing=main/Testing

--- Testing/update_tty2oled.sh
+++ Testing/update_tty2oled.sh
@@ -28,7 +28,8 @@
 
 
 #REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/main"
-REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/testing"    # Testing branch
+REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/main/Testing"
+#REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/testing"    # Testing branch
 
 SCRIPTNAME="/tmp/update_tty2oled_script.sh"
 NODEBUG="-q -o /dev/null"

3c764ef70890d49aa17512a8c13280be29ed5b5c
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-10-11 17:14:32 +0200

    Update update_tty2oled.sh

--- update_tty2oled.sh
+++ update_tty2oled.sh
@@ -29,7 +29,7 @@
 
 
 REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/main"
-#REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/testing"    # Testing branch
+#REPOSITORY_URL="https://raw.githubusercontent.com/venice1200/MiSTer_tty2oled/main/Testing"    # Testing branch
 
 SCRIPTNAME="/tmp/update_tty2oled_script.sh"
 NODEBUG="-q -o /dev/null"

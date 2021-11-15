53accbdea698945218651558640a5fd76373b956
venice1200 <43762187+venice1200@users.noreply.github.com>
2021-11-15 17:36:42 +0100

    Delete obsoletes...

--- Testing/fw_update/old/upload_testing.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-. /media/fat/tty2oled/tty2oled.ini
-
-esptool.py --chip esp32 --port ${TTYDEV} --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 MiSTer_SSD1322_USB.ino.bootloader.bin 0x10000 MiSTer_SSD1322_USB_Testing.ino.esp32.bin 0x8000 MiSTer_SSD1322_USB.ino.partitions.bin 

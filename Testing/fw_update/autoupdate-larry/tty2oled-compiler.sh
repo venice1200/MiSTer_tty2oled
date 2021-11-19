77bb3638a79940e0b656b2acac9f632715d98e38
Oliver Jaksch <ojaksch@com-in.de>
2021-11-19 13:01:50 +0100

    tty2oled-compiler

--- Testing/fw_update/autoupdate-larry/tty2oled-compiler.sh
+++ Testing/fw_update/autoupdate-larry/tty2oled-compiler.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
-SKETCHNAM=$(ls "${2}/"*.ino)
-SKETCHNAM="$(basename "${SKETCHNAM%.*}")"
+SKETCHNAME="${HOME}/Arduino/MiSTer_SSD1322_USB/MiSTer_SSD1322_USB.ino"
+SKETCHPATH="$(dirname "${SKETCHNAME%.*}")"
 BBUILDPATH=$(mktemp -d)
-SBUILDPATH=$(mktemp -d)
-SBUILDPATH="${SBUILDPATH}/${SKETCHNAM}"
-mkdir -p "${SBUILDPATH}"
+WWWPATH="/var/www/tty2tft.de/htdocs/MiSTer_tty2oled-installer"
 
-cp -a "${2}/"*.ino "${2}/"*.h "${SBUILDPATH}/"
-case "${1}" in
-    HWESP32DE) BOARD="esp32:esp32:esp32" ;;
-    HWLOLIN32) BOARD="esp32:esp32:lolin32" ;;
-    HWESP8266) BOARD="esp8266:esp8266:nodemcuv2:xtal=160,ssl=basic,baud=921600" ;;
-esac
+acompile() {
+    echo "Compiling ${BLA}...Please wait..."
+    time arduino-cli compile -b "${BOARD}" --build-path "${BBUILDPATH}" --clean "${SKETCHPATH}" --log-level info
+    BUILDNAM=${BLA,,} ; BUILDNAM=${BUILDNAM:2}
+    BUILDVER=$(grep "#define BuildVersion" ${BBUILDPATH}/sketch/*.ino.cpp | awk '{print $3}' | tr -d "\"")
+    cp -a "${BBUILDPATH}/$(basename ${SKETCHNAME}).bin" "${WWWPATH}/${BUILDNAM}_${BUILDVER}.bin"
+}
 
-echo "Compiling...Please wait..."
-time arduino-cli compile -b "${BOARD}" --build-path "${BBUILDPATH}" --clean "${SBUILDPATH}" --log-level info
+acompcopy() {
+    cp -a "${BBUILDPATH}/$(basename ${SKETCHNAME}).partitions.bin" "${WWWPATH}/partitions.bin"
+}
+
+for BLA in HWESP32DE HWLOLIN32 HWESP8266; do
+    case "${BLA}" in
+	HWESP32DE)
+	    BOARD="esp32:esp32:esp32"
+	    acompile && acompcopy
+	    ;;
+	HWLOLIN32)
+	    BOARD="esp32:esp32:lolin32"
+	    acompile && acompcopy
+	    ;;
+	HWESP8266)
+	    BOARD="esp8266:esp8266:nodemcuv2:xtal=160,ssl=basic,baud=921600"
+	    acompile
+	    ;;
+    esac
+done
+
+echo "${BUILDVER}" > "${WWWPATH}/buildver"
+cp -a ${HOME}/.arduino15/packages/esp32/hardware/esp32/*/tools/partitions/boot_app0.bin "${WWWPATH}/"
+cp -a ${HOME}/.arduino15/packages/esp32/hardware/esp32/*/tools/sdk/esp32/bin/bootloader_dio_80m.bin "${WWWPATH}/"
+cp -a ${HOME}/.arduino15/packages/esp32/hardware/esp32/*/tools/sdk/esp32/bin/bootloader_qio_80m.bin "${WWWPATH}/"

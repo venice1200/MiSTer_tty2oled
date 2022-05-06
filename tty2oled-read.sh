3f7d00fa0006e56b65d4d54a4daa5732fee5af07
Oliver Jaksch <ojaksch@com-in.de>
2022-05-06 11:57:29 +0200

    tty2oled-read.sh: tmp is tty2oled-read

--- tty2oled-read.sh
+++ tty2oled-read.sh
@@ -3,4 +3,4 @@
 . /media/fat/tty2oled/tty2oled-system.ini
 . /media/fat/tty2oled/tty2oled-user.ini
 
-[[ -c ${TTYDEV} ]] && cat ${TTYDEV} > /tmp/tty2oled-buffer
+[[ -c ${TTYDEV} ]] && cat ${TTYDEV} > /tmp/tty2oled-read

cf813684b37f6621b8bb2c919205ee90cf70b274
Oliver Jaksch <ojaksch@com-in.de>
2022-05-06 13:41:12 +0200

    tty2oled-read.sh: cat to background

--- tty2oled-read.sh
+++ tty2oled-read.sh
@@ -3,4 +3,5 @@
 . /media/fat/tty2oled/tty2oled-system.ini
 . /media/fat/tty2oled/tty2oled-user.ini
 
-[[ -c ${TTYDEV} ]] && cat ${TTYDEV} > /tmp/tty2oled-read
+# [[ -c ${TTYDEV} ]] && cat ${TTYDEV} > /tmp/tty2oled-read
+[[ -c ${TTYDEV} ]] && cat ${TTYDEV} > /tmp/tty2oled-read & echo $! > /run/tty2oled-read.pid

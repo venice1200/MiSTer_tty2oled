72e753144ee03ce352995045555628e9f923f7b8
Oliver Jaksch <ojaksch@com-in.de>
2022-05-07 07:14:33 +0200

    Updated tty2oled-read

--- tty2oled-read.sh
+++ tty2oled-read.sh
@@ -3,5 +3,8 @@
 . /media/fat/tty2oled/tty2oled-system.ini
 . /media/fat/tty2oled/tty2oled-user.ini
 
-# [[ -c ${TTYDEV} ]] && cat ${TTYDEV} > /tmp/tty2oled-read
-[[ -c ${TTYDEV} ]] && cat ${TTYDEV} > /tmp/tty2oled-read & echo $! > /run/tty2oled-read.pid
+if [[ -c ${TTYDEV} ]]; then
+  cat ${TTYDEV} > /tmp/tty2oled-read & echo $! > /run/tty2oled-read.pid
+fi
+
+# kill $(cat /run/tty2oled-read.pid) && rm /run/tty2oled-read.pid && > /tmp/tty2oled-read

3de0a20afd53f9ba90d67089b8d62d5c4e12cd54
Oliver Jaksch <ojaksch@com-in.de>
2022-08-19 15:02:09 +0200

    Installer: don't cls

--- installer.sh
+++ installer.sh
@@ -48,7 +48,7 @@ fi
 [ "${TTY2OLED_FW_TESTING}" = "yes" ] && BUILDVER=$(wget -q ${REPOSITORY_URL2}/buildverT -O -) || BUILDVER=$(wget -q ${REPOSITORY_URL2}/buildver -O -)
 
 # Clear the display by setting this as CORENAME, which keeps the display while updating
-echo "cls" > /tmp/CORENAME
+#echo "cls" > /tmp/CORENAME
 
 # Stop an already running daemon
 if [ $(pidof ${DAEMONNAME}) ] && [ -f ${INITSCRIPT} ] ; then

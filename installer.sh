42ad35926b29cb5e23f7c3dfffdfd4d187f92ec1
Oliver Jaksch <ojaksch@com-in.de>
2022-11-23 15:27:58 +0100

    Installer: ESP32S3 added

--- installer.sh
+++ installer.sh
@@ -15,6 +15,10 @@ flash() {
 	    wget -q ${REPOSITORY_URL2}/MAC.html?${MAC} ${REPOSITORY_URL2}/boot_app0.bin ${REPOSITORY_URL2}/bootloader_dio_80m.bin ${REPOSITORY_URL2}/partitions.bin ${REPOSITORY_URL2}/esp32de_${BUILDVER}.bin
 	    ${TMPDIR}/esptool.py --chip esp32 --port ${TTYDEV} --baud ${DBAUD} ${DSTD} 0xe000 ${TMPDIR}/boot_app0.bin 0x1000 ${TMPDIR}/bootloader_dio_80m.bin 0x10000 ${TMPDIR}/esp32de_${BUILDVER}.bin 0x8000 ${TMPDIR}/partitions.bin
 	    ;;
+	HWESP32S3)
+	    wget -q ${REPOSITORY_URL2}/MAC.html?${MAC} ${REPOSITORY_URL2}/boot_app0.bin ${REPOSITORY_URL2}/bootloader_dio_80m.bin ${REPOSITORY_URL2}/partitions.bin ${REPOSITORY_URL2}/esp32s3_${BUILDVER}.bin
+	    ${TMPDIR}/esptool.py --chip esp32 --port ${TTYDEV} --baud ${DBAUD} ${DSTD} 0xe000 ${TMPDIR}/boot_app0.bin 0x1000 ${TMPDIR}/bootloader_dio_80m.bin 0x10000 ${TMPDIR}/esp32s3_${BUILDVER}.bin 0x8000 ${TMPDIR}/partitions.bin
+	    ;;
 	HWLOLIN32 | HWDTIPCB0 | HWDTIPCB1)
 	    wget -q ${REPOSITORY_URL2}/MAC.html?${MAC} ${REPOSITORY_URL2}/boot_app0.bin ${REPOSITORY_URL2}/bootloader_dio_80m.bin ${REPOSITORY_URL2}/partitions.bin ${REPOSITORY_URL2}/lolin32_${BUILDVER}.bin
 	    ${TMPDIR}/esptool.py --chip esp32 --port ${TTYDEV} --baud ${DBAUD} ${DSTD} 0xe000 ${TMPDIR}/boot_app0.bin 0x1000 ${TMPDIR}/bootloader_dio_80m.bin 0x10000 ${TMPDIR}/lolin32_${BUILDVER}.bin 0x8000 ${TMPDIR}/partitions.bin
@@ -91,6 +95,7 @@ if [ "${MCUtype}" = "" ]; then
 	    --backtitle "tty2oled" --title "[ Flash tool for ESP devices ]" \
 	    --menu "Use the arrow keys and enter \nor the d-pad and A button" 0 0 0 \
 	    HWESP32DE "TTGO/DTI (ESP32)" \
+	    HWESP32S3 "ESP32-S3 DevKitC (ESP32-S3)" \
 	    HWLOLIN32 "Wemos/Lolin/DevKit_V4 (ESP32)" \
 	    HWESP8266 "NodeMCU v3 (ESP8266)" \
 	    Exit "Exit now" 2>&1 1>&3)
@@ -109,6 +114,7 @@ case "${MCUtype}" in
     Exit)	exit 0 ;;
     HWNONEXXX)	echo -e "${fred}Unknown hardware, can't continue.${freset}" ; exit 1 ;;
     HWESP32DE)	echo -e "${fyellow}ESP32 selected/detected (TTGO/DTI).${freset}" ;;
+    HWESP32S3)	echo -e "${fyellow}ESP32 selected/detected (ESP32-S3 DevKitC).${freset}" ;;
     HWLOLIN32)	echo -e "${fyellow}ESP32 selected/detected (Wemos/Lolin/DevKit_V4).${freset}" ;;
     HWDTIPCB0)	echo -e "${fyellow}ESP32 selected/detected (DTI v1.0).${freset}" ;;
     HWDTIPCB1)	echo -e "${fyellow}ESP32 selected/detected (DTI n/a).${freset}" ;;

afcb5983698603399aeb0ffffaa09412a9cf18b1
Oliver Jaksch <ojaksch@com-in.de>
2023-01-19 08:46:03 +0100

    AutoUpdate

--- Testing/update_tty2oled_script.sh
+++ Testing/update_tty2oled_script.sh
@@ -172,12 +172,14 @@ fi
 # Check and remount root non-writable if neccessary
 [ "${MOUNTRO}" = "true" ] && /bin/mount -o remount,ro /
 
-if [ $(pidof ${DAEMONNAME}) ]; then
-  echo -e "${fgreen}Restarting init script\n${freset}"
-  ${INITSCRIPT} restart
-elif [ -c "${TTYDEV}" ]; then
-  echo -e "${fgreen}Starting init script\n${freset}"
-  ${INITSCRIPT} start
+if [ "${1}" != "NOINSTALLER" ]; then
+  if [ $(pidof ${DAEMONNAME}) ]; then
+    echo -e "${fgreen}Restarting init script\n${freset}"
+    ${INITSCRIPT} restart
+  elif [ -c "${TTYDEV}" ]; then
+    echo -e "${fgreen}Starting init script\n${freset}"
+    ${INITSCRIPT} start
+  fi
 fi
 
 [ -z "${SSH_TTY}" ] && echo -e "${fgreen}Press any key to continue\n${freset}"

9499b27dca28df3e8f6dd00a80e758555ff2833f
Oliver Jaksch <ojaksch@com-in.de>
2023-03-03 13:54:03 +0100

    installer/testing: wait for internet for max. 60s

--- Testing/update_auto.sh
+++ Testing/update_auto.sh
@@ -4,5 +4,5 @@
 . /media/fat/tty2oled/tty2oled-user.ini
 
 echo -e "Waiting for Internet access: "
-until ping -W1 -c1 8.8.8.8 >/dev/null 2>&1; do echo -n "." ; sleep 0.2; done
+for WAIT4NET in {1..50}; do ping -W1 -c1 192.168.9.9 >/dev/null 2>&1; echo -n "." ; sleep 0.2; done
 bash ${UPDATESCRIPT} NOINSTALLER > /dev/null 2>&1	# Auto Update after 1 week
